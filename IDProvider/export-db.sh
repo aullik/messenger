05362bfd9a0e2b1eebfa1fc827d4cfb6cfe21e6c
patrick sterk <patrick.sterk@protonmail.com>
2020-04-17 13:40:25 +0200

    fix user export/import

---
+++ IDProvider/export-db.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+docker exec -it keycloak /opt/jboss/keycloak/bin/standalone.sh \
+-Djboss.socket.binding.port-offset=100 -Dkeycloak.migration.action=export \
+-Dkeycloak.migration.provider=singleFile \
+-Dkeycloak.migration.realmName=SpringBootKeycloak \
+-Dkeycloak.migration.usersExportStrategy=REALM_FILE \
+-Dkeycloak.migration.file=/tmp/my_realm.json
--- start_db_cluster_and_monitor.sh
+++ start_db_cluster_and_monitor.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-docker-compose -f metric/docker-compose.yml -f database/docker-compose.yml -p proj up
+docker-compose -f metric/docker-compose.yml -f database/docker-compose.yml -f IDProvider/docker-compose.yml -f docker-compose.dev.yml -p proj up
 cd metric && docker-compose down && cd ..
 cd database && docker-compose down && cd ..
+cd IDProvider && docker-compose down && cd ..
