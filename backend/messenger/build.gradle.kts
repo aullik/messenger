246ab475222f2e7c5f0fdea4f04c7b3d9b003e84
aullik <nicolaswehrle@gmx.net>
2020-05-03 18:41:43 +0200

    update build file. Add dgraph and minio client

--- backend/messenger/build.gradle.kts
+++ backend/messenger/build.gradle.kts
@@ -7,9 +7,6 @@ repositories {
     mavenCentral()
     jcenter()
     mavenLocal()
-    maven {
-        url = uri("http://repo.maven.apache.org/maven2")
-    }
 }
 
 sourceSets {
@@ -27,6 +24,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux:2.2.6.RELEASE")
     implementation("org.springframework.session:spring-session-core:2.2.2.RELEASE")
     implementation("org.keycloak:keycloak-spring-boot-starter:9.0.2")
+    implementation("io.dgraph:dgraph4j:20.03.0")
+    implementation("io.minio:minio:7.0.2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.2.6.RELEASE")
     testImplementation("io.projectreactor:reactor-test:3.3.4.RELEASE")
 }
@@ -39,13 +39,6 @@ java {
     sourceCompatibility = org.gradle.api.JavaVersion.VERSION_1_8
 }
 
-//publishing {
-//    publications {
-//        create<MavenPublication>("maven") {
-//            from(components["java"])
-//        }
-//    }
-//}
 
 tasks.withType<JavaCompile> {
     options.encoding = ("UTF-8")
